1417ba378b1e1a5a11ae0f0ccd5f65e68c4d0716
Foma Mironenko <fmironenko@reflexion.com>
2023-12-11 22:45:33 +0300

    [ADD]: submit sort via radix sort

--- sort/sort.cpp
+++ sort/sort.cpp
@@ -3,91 +3,68 @@
 #include <cstring>
 #include <cassert>
 #include <cstdint>
-
-constexpr size_t  MAX_INSERT_SORT_SIZE = (1 << 3);
-constexpr size_t  MAX_SORT_SIZE = (1 << 25);
+#include <limits>
 
 
-inline uint16_t lowerBound(const int * data, uint16_t size, int val)
+void radixSort(std::vector<uint32_t> & data)
 {
-    uint16_t lhs = 0, rhs = size, mid = 0;
-    while (lhs < rhs) {
-        mid = (rhs + lhs) / 2;
-        if (data[mid] < val) {
-            lhs = mid + 1;
-        } else {
-            rhs = mid;
-        }
+    // Any positive NUM_DIGITS would work.
+    // Changing NUM_DIGITS means switching to a positional system with another base.
+    constexpr size_t NUM_DIGITS = 30;
+    constexpr size_t MAX_NUMBER = std::numeric_limits<uint32_t>::max();
+
+    std::vector<uint32_t> digits[NUM_DIGITS];
+    for (auto & digit_batch : digits) {
+        digit_batch.reserve(data.size());
     }
-    return lhs;
-}
 
-
-void insertion_sort(int * data, uint32_t size)
-{
-    static int buffer[MAX_INSERT_SORT_SIZE];
-
-    int cur_elt = 0;
-    uint16_t insert_pos = 0, bytes_to_move;
-    for (uint16_t cur_size = 0; cur_size < size; ++cur_size) {
-
-        cur_elt = data[cur_size];
-        insert_pos = lowerBound(buffer, cur_size, cur_elt);
-        // assert(insert_pos <= cur_size);
-
-        bytes_to_move = sizeof(int) * (cur_size - insert_pos);
-        if (bytes_to_move > 0) {
-            // Ranges in memmove can intersect
-            memmove(buffer + insert_pos + 1, buffer + insert_pos, bytes_to_move);
+    uint32_t cur_digit = 0;
+    for (size_t base_pow = 1; base_pow < MAX_NUMBER; base_pow *= NUM_DIGITS) {
+        
+        // Distribute elements into batches
+        size_t idx = 0;
+        for (; idx < data.size(); ++idx) {
+            cur_digit = (data[idx] / base_pow) % NUM_DIGITS;
+            digits[cur_digit].push_back(data[idx]);
         }
 
-        buffer[insert_pos] = cur_elt;
-
-    }
-    memcpy(data, buffer, sizeof(int) * size);
-}
-
-
-void sort(int * data, uint32_t size)
-{
-    // insertion sort
-    if (size <= MAX_INSERT_SORT_SIZE) {
-        insertion_sort(data, size);
-        return;
-    }
-
-    // merge sort
-    static int buffer[MAX_SORT_SIZE];
-    uint32_t middle = size / 2;
-
-    sort(data, middle);
-    sort(data + middle, size - middle);
-
-    uint32_t lhs_i = 0, lhs_max = middle;
-    uint32_t rhs_i = middle, rhs_max = size;
-    uint32_t dst_i = 0;
-
-    while (lhs_i < lhs_max && rhs_i < rhs_max) {
-        if (data[lhs_i] <= data[rhs_i]) {
-            buffer[dst_i++] = data[lhs_i++];
-        } else {
-            buffer[dst_i++] = data[rhs_i++];
+        // Gather back into data
+        idx = 0;
+        for (auto & digit_batch : digits) {
+            for (uint32_t value : digit_batch) {
+                data[idx++] = value;
+            }
+            digit_batch.clear();
         }
-    }
-    // Only one will take place
-    if (lhs_i < lhs_max) {
-        memcpy(buffer + dst_i, data + lhs_i, sizeof(int) * (lhs_max - lhs_i));
-    }
-    if (rhs_i < rhs_max) {
-        // Needn't copy since the values are already there
-        size = dst_i;
-    }
 
-    memcpy(data, buffer, sizeof(int) * size);
+    }
 }
 
 
 void sort(std::vector<int>& vec)
 {
-    sort(vec.data(), vec.size());
+    std::vector<uint32_t> negative;
+    std::vector<uint32_t> positive;
+    negative.reserve(vec.size());
+    positive.reserve(vec.size());
+
+    for (int element : vec) {
+        if (element < 0) {
+            assert(element != std::numeric_limits<int>::min());
+            negative.push_back(static_cast<uint32_t>( -element ));
+        } else {
+            positive.push_back(static_cast<uint32_t>(  element ));
+        }
+    }
+
+    radixSort(negative);
+    radixSort(positive);
+
+    vec.clear();
+    for (auto rneg = negative.crbegin(); rneg != negative.crend(); ++rneg) {
+        vec.push_back(-static_cast<int>(*rneg));
+    }
+    for (auto pos = positive.cbegin(); pos != positive.cend(); ++pos) {
+        vec.push_back(static_cast<int>(*pos));
+    }    
 }
